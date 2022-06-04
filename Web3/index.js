558cfa1c604486888a8c815d9c53bbc7a3daa186
DhanteyUD <otseclinton60@gmail.com>
2022-06-04 12:47:22 +0100

    Updated Web3

--- Web3/index.js
+++ Web3/index.js
@@ -1,13 +1,11 @@
 const Web3 = require('web3');
-const provider = new Web3.providers.HttpProvider(
-  'https://mainnet.infura.io/v3/8414250e9b6e4e5ba9ee634ecd0525b2'
-);
+const dotenv = require('dotenv');
+dotenv.config();
+const provider = new Web3.providers.HttpProvider(process.env.INFURA_URL);
 const web3 = new Web3(provider);
 
 const getBalance = async () => {
-  const result = await web3.eth.getBalance(
-    '0x9b17D02Fc2e64e797aa052a6209eF8D1b85fC49D'
-  );
+  const result = await web3.eth.getBalance(process.env.WALLET_ADDRESS);
   const balance = web3.utils.fromWei(result, 'ether');
   console.log(balance);
 };
